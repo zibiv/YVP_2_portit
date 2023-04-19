15f731a07223d93c4e6ca9bba2e456f0af4f6288
GenIl <inmydreamsin@gmail.com>
2023-04-19 09:36:45 +0300

    Добавлена мутация для отправки запроса на удаление на сервер Обработчик запускающий удаление передан в компонент Toggle

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -4,48 +4,76 @@ import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 import Toggle from "./Toggle"
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios"
+import { error } from "console"
 
 type EditProps = {
-  id: string;
-  userPick: string;
-  name: string;
-  title: string;
+  id: string
+  userPick: string
+  name: string
+  title: string
   comments: {
-    id: string;
-    userId: string;
-    postId: string;
+    id: string
+    userId: string
+    postId: string
   }[]
 }
 
-export default function EditPost({ userPick, name, title, comments, id }: EditProps) {
+export default function EditPost({
+  userPick,
+  name,
+  title,
+  comments,
+  id,
+}: EditProps) {
   const [toggle, setToggle] = useState(false)
+
+  const mutation = useMutation(
+    (id: string) => axios.delete("/api/posts/deletePost", { data: { id } }),
+    {
+      onError: (error) => {
+        console.log(error)
+      },
+      onSuccess: (data) => {
+        console.log(data)
+        setToggle(false)
+      },
+    }
+  )
+
+  function handleClickDelete () {
+    mutation.mutate(id)
+  }
+
   return (
     <>
-    <div className="bg-white my-2 p-8 rounded-xl shadow-sm">
-      <div className="flex items-center gap-2 ">
-        <Image
-          className="rounded-full"
-          src={userPick}
-          alt="user picture"
-          width={32}
-          height={32}
-        />
-        <h3 className="text-teal-700 font-bold">{name}</h3>
+      <div className="bg-white my-2 p-8 rounded-xl shadow-sm">
+        <div className="flex items-center gap-2 ">
+          <Image
+            className="rounded-full"
+            src={userPick}
+            alt="user picture"
+            width={32}
+            height={32}
+          />
+          <h3 className="text-teal-700 font-bold">{name}</h3>
+        </div>
+        <div className="my-8">
+          <p className="break-all">{title}</p>
+        </div>
+        <div className="flex gap-4 items-center">
+          <Link href={`/post/${id}`}>
+            <p className="text-sm font-bold text-gray-700">
+              Comments: {comments.length}
+            </p>
+          </Link>
+          <button className=" text-red-500 " onClick={() => setToggle(!toggle)}>
+            Удалить
+          </button>
+        </div>
       </div>
-      <div className="my-8">
-        <p className="break-all">{title}</p>
-      </div>
-      <div className="flex gap-4 items-center">
-        <Link href={`/post/${id}`}>
-          <p className="text-sm font-bold text-gray-700">Comments: {comments.length}</p>
-        </Link>
-        <button
-          className=" text-red-500 "
-          onClick={() => setToggle(!toggle)}
-        >Удалить</button>
-      </div>
-    </div>
-    { toggle && <Toggle setToggle={setToggle}/> }
+      {toggle && <Toggle setToggle={setToggle} deletePost={handleClickDelete}/>}
     </>
   )
 }
--- app/dashboard/Toggle.tsx
+++ app/dashboard/Toggle.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-export default function Toggle({
-  setToggle,
-}: {
+type ToggleProps = {
   setToggle: (fn: (a: boolean) => boolean) => void
-}) {
+  deletePost: () => void
+}
+
+export default function Toggle({ setToggle, deletePost }: ToggleProps) {
   function closeToggle() {
     setToggle((prev: boolean) => !prev)
   }
@@ -15,18 +16,29 @@ export default function Toggle({
         closeToggle()
       }}
     >
-      <div className="absolute bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-3 text-center"
-      onClick={(e) => e.stopPropagation()}>
+      <div
+        className="absolute bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-3 text-center"
+        onClick={(e) => e.stopPropagation()}
+      >
         <h1>Вы действительно хотите удалить этот пост?</h1>
-        <h3 className="text-red-600 text-sm text-center"> запись нельзя будет восстановить </h3>
+        <h3 className="text-red-600 text-sm text-center">
+          {" "}
+          запись нельзя будет восстановить{" "}
+        </h3>
         <div className="mt-4 flex flex-col gap-3">
-          <button className=" text-blue-500" onClick={(e) => {
-            e.stopPropagation();
-            closeToggle()}
-          }>
+          <button
+            className=" text-blue-500"
+            onClick={(e) => {
+              e.stopPropagation()
+              closeToggle()
+            }}
+          >
             отмена
           </button>
-          <button className="bg-red-600 text-sm text-white py-2 px-4 rounded-md" >
+          <button
+            className="bg-red-600 text-sm text-white py-2 px-4 rounded-md"
+            onClick={deletePost}
+          >
             удалить
           </button>
         </div>
