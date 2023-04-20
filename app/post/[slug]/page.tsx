322e8e70deeec0c156c3a63c27b0303aab02c858
GenIl <inmydreamsin@gmail.com>
2023-04-20 11:00:29 +0300

    Отображение поста на странице поста

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import Post from "@/app/componets/Post";
-import { useQuery } from "@tanstack/react-query";
-import { type Post as PostType } from "@/app/types/Posts";
-import axios from "axios";
-import Loader from "@/app/componets/Loader";
+import Post from "@/app/componets/Post"
+import { useQuery } from "@tanstack/react-query"
+import { type Post as PostType } from "@/app/types/Posts"
+import axios from "axios"
+import Loader from "@/app/componets/Loader"
 
 type URL = {
   params: {
@@ -18,18 +18,25 @@ async function fetchPostDetail(slug: string) {
 }
 
 export default function PostDetail(url: URL) {
-  const { data, isLoading, error } = useQuery<PostType[]>({
+  const { data, isLoading, error } = useQuery<PostType>({
     queryKey: ["postDetail", url.params.slug],
-    queryFn: () => fetchPostDetail(url.params.slug)
+    queryFn: () => fetchPostDetail(url.params.slug),
   })
-  
-  if(isLoading) return <Loader />;
+
+  if (isLoading) return <Loader />
   console.log(data)
-  
+
+  const props = {
+    userPick: data!.user.image,
+    name: data!.user.name,
+    postTitle: data!.title,
+    id: data!.id,
+    comments: data!.comments,
+  }
 
   return (
     <div>
-
+      <Post key={data?.id} {...props} />
     </div>
   )
 }
